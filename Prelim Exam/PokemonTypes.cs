013440b0a50e17db1a71adfef772eecb74c738ec
Michaela <145468590+MikaLouise@users.noreply.github.com>
2024-02-26 23:49:14 +0800

    Add files via upload

---
+++ Prelim Exam/PokemonTypes.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Role { Tank, Mage, Fighter, Marksman, Support, Assassin }
+public enum Specialty { CrowdControl, Charge, Reap, Burst, Damage }
