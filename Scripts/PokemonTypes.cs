d2b99cef9ea90fd5b243c0aa755e349828f14244
Michaela <145468590+MikaLouise@users.noreply.github.com>
2024-02-28 23:31:31 +0800

    Delete Scripts directory

--- Scripts/PokemonTypes.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum Role { Tank, Mage, Fighter, Marksman, Support, Assassin }
-public enum Specialty { CrowdControl, Charge, Reap, Burst, Damage }
